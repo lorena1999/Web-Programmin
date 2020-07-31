62654a0c381a81de7341a4fa731e21d3fca62337
lorena1999 <lorena.marginean@yahoo.com>
2020-07-31 14:55:05 +0300

    Projects Web 2

--- WebApp_Servlet_JSP/src/dbLayer/MyProvider.java
+++
@@ -1,7 +0,0 @@
-package dbLayer;
-
-public interface MyProvider {
-    String username = "root";
-    String password = "";
-    String connUrl = "jdbc:mysql://localhost:10245/examweb?useSSL=false";
-}
